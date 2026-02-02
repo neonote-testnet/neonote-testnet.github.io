209ce0d77f12163bb91cefe363f122d415f605fd
neonote-testnet <bossweki@gmail.com>
2026-02-02 12:13:58 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE = 'neonote-v170'; 
+const CACHE = 'neonote-v171';
+
 const ASSETS = [
   './',
   './index.html',
@@ -13,18 +14,20 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting(); 
+
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
       )
     )
   );
-  self.clients.claim();
+
 });
 
 self.addEventListener('fetch', e => {
